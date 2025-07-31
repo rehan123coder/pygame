e0609a3baab486dfd4c8b0c8beaa2e1ff2b7b579
rehan123coder <rehankhann@gmail.com>
2025-07-31 16:14:56 +0500

    pygame

--- first.py
+++ first.py
@@ -1,17 +1,43 @@
 import pygame
-pygame.init()
-SH,SW=500,500
-screen=pygame.display.set_mode((SH,SW))
-pygame.display.set_caption("my first game")
-screen.fill((255,255,255))
-green=(0,255,0)
-done=False
-while not done:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-    pygame.draw.rect(screen,(125,75,35),pygame.Rect(30,30,100,100))
-    pygame.draw.rect(screen,(125,75,35),pygame.Rect(130,130,100,100),5)
-    pygame.draw.circle(screen,green,(300,300),50)
-    pygame.draw.circle(screen,green,(400,400),50,5)
-    pygame.display.flip()
+def main (): 
+    pygame.init()
+    SH,SW=500,500
+    screen=pygame.display.set_mode((SH,SW))
+    pygame.display.set_caption("my first game")
+    colors ={
+        'red':pygame.color('red'),
+        'green':pygame.color('green'),
+        'blue':pygame.color('blue'),
+        'yellow':pygame.color('yellow'),
+        'white':pygame.color('white')
+    }
+    current__colour=colors('white')
+    x,y=30,30
+    spw,sph=60,60
+    clock= pygame.time.Clock()
+    done=False
+    while not done :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done=True    
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]: x -=3
+        if pressed[pygame.K_RIGHT]: x +=3
+        if pressed[pygame.K_UP]: y -=3
+        if pressed[pygame.K_DOWN]: y +=3
+        x=min (max(0,x)),SW-spw
+        y=min (max(0,y)),SH-sph
+        if x == 0 : current__colour =colors ['blue']
+        elif x ==SW-spw:current__colour =colors ['yellow']
+        elif x == 0 : current__colour =colors ['red']
+        elif y ==SH-sph:current__colour =colors ['green']
+        else:
+            current__colour=colors('white')
+        screen.fill((255,255,255))
+        pygame.draw.rect(screen,current__colour,
+                        x,y,spw,sph )
+        pygame.display.flip()
+        clock.tick(90)
+    pygame.quit()
+if __name__=="__main__":
+    main()
--- pygame3.py
+++ pygame3.py
@@ -2,12 +2,12 @@ import pygame
 pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
 display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Adding image and background image')
+pygame.display.set_caption('im zohan')
 background_image = pygame.transform.scale(
     pygame.image.load('background.png').convert(),
     (SCREEN_WIDTH, SCREEN_HEIGHT))
 penguin_image = pygame.transform.scale(
-    pygame.image.load('cute.png').convert_alpha(), (200, 200))
+    pygame.image.load('QA2_7224.JPG').convert_alpha(), (200, 200))
 penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH // 2,
                                               SCREEN_HEIGHT // 2 - 30))
 text = pygame.font.Font(None, 36).render('Hello World ', True,
